212d342027a70f7dd3ca05886c7f7dd29d02e4ce
marshallpeters5 <89714815+marshallpeters5@users.noreply.github.com>
2023-06-18 01:14:19 -0400

    added reset.css and commented

--- public/javascript/background.js
+++ public/javascript/background.js
@@ -1,13 +1,26 @@
+// Select the checkbox element
 const checkbox = document.querySelector("#checkbox");
+
+// Select the mainBody element
 const mainBody = document.querySelector(".mainBody");
 
-checkbox.addEventListener("click", () => mainBody.classList.toggle("darkBg"));
-$(document).ready(function() {
-    $('#checkbox').change(function() {
-       if ($(this).is(':checked')) {
-          $('body').addClass('dark-mode');
-       } else {
-          $('body').removeClass('dark-mode');
-       }
+// Add a click event listener to the checkbox
+checkbox.addEventListener("click", () => {
+    // Toggle the 'darkBg' class on the mainBody element
+    mainBody.classList.toggle("darkBg");
+});
+
+// When the document is ready
+$(document).ready(function () {
+    // Add a change event listener to the checkbox using jQuery
+    $('#checkbox').change(function () {
+        // If the checkbox is checked
+        if ($(this).is(':checked')) {
+            // Add the 'dark-mode' class to the body element
+            $('body').addClass('dark-mode');
+        } else {
+            // Remove the 'dark-mode' class from the body element
+            $('body').removeClass('dark-mode');
+        }
     });
- });
+});
--- public/javascript/cards.js
+++ public/javascript/cards.js
@@ -1,18 +1,28 @@
-$(document).ready(function() {
-    $(".option").click(function() {
-      $(".option").removeClass("active");
-      $(this).addClass("active");
-    });
-  });
+// When the document is ready
+$(document).ready(function () {
+  // Add a click event listener to elements with the class "option" using jQuery
+  $(".option").click(function () {
+    // Remove the "active" class from all elements with the class "option"
+    $(".option").removeClass("active");
 
-  const options = document.querySelectorAll('.option');
-
-  options.forEach(option => {
-    option.addEventListener('click', function(event) {
-      if (option.classList.contains('active')) {
-        // Allow click event to proceed
-      } else {
-        event.preventDefault(); // Prevent the default link behavior
-      }
-    });
+    // Add the "active" class to the clicked element
+    $(this).addClass("active");
   });
+});
+
+// Select all elements with the class "option"
+const options = document.querySelectorAll('.option');
+
+// Iterate over each option
+options.forEach(option => {
+  // Add a click event listener to each option
+  option.addEventListener('click', function (event) {
+    // Check if the clicked option already has the class "active"
+    if (option.classList.contains('active')) {
+      // Do nothing if it is already active
+    } else {
+      // Prevent the default behavior of the click event
+      event.preventDefault();
+    }
+  });
+});
